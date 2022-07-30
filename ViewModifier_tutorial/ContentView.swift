48114907616632e17951fcb4a6582eb9e9092738
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-31 04:19:19 +0900

    viewModifier

--- ViewModifier_tutorial/ContentView.swift
+++ ViewModifier_tutorial/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 50) {
+            Text("Hello, world!")
+                .modifier(MyRoundedText())
+            
+            Text("Hello, world!")
+                .myRoundedTextStyle()
+            
+            Image(systemName: "pencil")
+                .myRoundedTextStyle()
+            
+            Rectangle().frame(width: 100, height: 100, alignment: .center)
+                .myRoundedTextStyle()
+                
+        }
+    }
+}
+
+struct MyRoundedText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
             .padding()
+            .background(Color.cyan)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth:10)
+            .foregroundColor(.blue))
+    }
+}
+
+extension View {
+    func myRoundedTextStyle() -> some View {
+        modifier(MyRoundedText())
     }
 }
 
